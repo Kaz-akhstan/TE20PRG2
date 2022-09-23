2c0dbe0f25f04ae3b6f308f7deba203a72eb8f04
Kaz-akhstan <77719351+Kaz-akhstan@users.noreply.github.com>
2022-09-23 13:38:26 +0200

    Det funkar nästan

---
+++ src/CryptGUI.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class CryptGUI {
+    private JTextField textField1;
+    private JButton button1;
+    private JTextField textField2;
+    private JButton button2;
+    private JTextField textField3;
+    private JButton button3;
+
+    public static void main(String[] args) {
+
+    }
+}
+
